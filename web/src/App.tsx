77fba4575a01ad9f9a0077e1e42c19a86ba22f1b
Tarun Kumar <100301640+kumartarun5678@users.noreply.github.com>
2024-08-05 16:24:33 +0530

    Create App.tsx

---
+++ web/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import TaskBoard from './components/TaskBoard';
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <TaskBoard />
+    </div>
+  );
+};
+
+export default App;
